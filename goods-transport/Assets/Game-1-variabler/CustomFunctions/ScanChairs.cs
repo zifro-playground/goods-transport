14a59c415724fb73b268011e916e19270bc0b4c7
jontebackis4 <jonathan.backstrom95@gmail.com>
2017-11-29 20:04:55 +0100

    Create scan chairs function. Add scanner rotation with red light. Add another level to represent the last type of level.

---
+++ goods-transport/Assets/Game-1-variabler/CustomFunctions/ScanChairs.cs
@@ -0,0 +1,25 @@
+﻿using Compiler;
+using UnityEngine;
+
+public class ScanChairs : Compiler.Function 
+{
+	public ScanChairs()
+	{
+		this.name = "scanna_antal_stolar";
+		this.inputParameterAmount.Add(0);
+		this.pauseWalker = true;
+		this.hasReturnVariable = false;
+	}
+
+	public override Variable runFunction(Scope currentScope, Variable[] inputParas, int lineNumber)
+	{
+		LevelController controller = GameObject.FindGameObjectWithTag("LevelController").GetComponent<LevelController>();
+
+		GameObject carToScan = controller.activeCars.First.Value;
+		GameObject.FindGameObjectWithTag("Scanner").GetComponent<Scanner>().Scan(carToScan);
+
+		int chairCount = 0;
+
+		return new Variable("chaircount", chairCount);
+	}
+}
---
+++ goods-transport/Assets/Game-1-variabler/Scripts/Scanner.cs
@@ -0,0 +1,46 @@
+﻿using PM;
+using UnityEngine;
+
+public class Scanner : MonoBehaviour, IPMCompilerStopped
+{
+	public GameObject scannerLamp;
+
+	private Vector3 targetPos;
+	private bool isScanning;
+	private float speed = 1;
+	
+	void Start () 
+	{
+		scannerLamp.SetActive(false);
+	}
+	
+	void Update ()
+	{
+		if (isScanning)
+		{
+			Vector3 targetDir = targetPos - scannerLamp.transform.position;
+			float step = speed * Time.deltaTime;
+			Vector3 newDir = Vector3.RotateTowards(scannerLamp.transform.forward, targetDir, step, 0);
+			
+			scannerLamp.transform.rotation = Quaternion.LookRotation(newDir);
+		}
+	}
+
+	public void Scan(GameObject obj)
+	{
+		isScanning = true;
+
+		Bounds carBounds = obj.GetComponent<Renderer>().bounds;
+		float carFront = carBounds.max.x;
+		float carRear = carBounds.min.x;
+
+		scannerLamp.SetActive(true);
+		scannerLamp.transform.LookAt(new Vector3(carFront, 0, 0));
+		targetPos = new Vector3(carRear, 0, 0);
+	}
+
+	public void OnPMCompilerStopped(HelloCompiler.StopStatus status)
+	{
+
+	}
+}
