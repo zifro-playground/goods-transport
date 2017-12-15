1d888e49e58e54868a78f496b4cfe81547e34033
Jonathan Bäckström <jonathan.backstrom95@gmail.com>
2017-12-15 16:12:42 +0100

    Add CheckBattery and ChargeCar functions.

---
+++ goods-transport/Assets/Game-2-if/CustomFunctions/CheckBattery.cs
@@ -0,0 +1,21 @@
+﻿using Compiler;
+using UnityEngine;
+
+public class CheckBattery : Compiler.Function
+{
+	public CheckBattery()
+	{
+		this.name = "kolla_batterinivå";
+		this.inputParameterAmount.Add(0);
+		this.hasReturnVariable = true;
+		this.pauseWalker = false;
+	}
+
+	public override Variable runFunction(Scope currentScope, Variable[] inputParas, int lineNumber)
+	{
+		int batteryLevel = 100;
+		Debug.Log("Kollar batteriet... Nuvarande nivå är: " + batteryLevel);
+
+		return new Variable("BatteryLevel", batteryLevel);
+	}
+}
