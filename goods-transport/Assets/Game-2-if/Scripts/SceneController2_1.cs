a8eecd326082d8590c92d00d07a71619c2572192
cire96 <ericl3@kth.se>
2018-04-19 20:35:29 +0200

    Update check

--- goods-transport/Assets/Game-2-if/Scripts/SceneController2_1.cs
+++ goods-transport/Assets/Game-2-if/Scripts/SceneController2_1.cs
@@ -1,12 +1,14 @@
-﻿using PM;
+using PM;
 using UnityEngine;
 
 public class SceneController2_1 : MonoBehaviour, ISceneController, IPMCompilerStopped, IPMCompilerStarted
 {
     public static int correctlyCharged = 0;
     public static int falselyCharged = 0;
+	public static int checkChargeCounter = 0;
 
     private int carsToCharge;
+	private int carsToCheck;
 
     public void SetPrecode(Case caseData)
 	{
@@ -29,15 +31,41 @@ public class SceneController2_1 : MonoBehaviour, ISceneController, IPMCompilerSt
         carsToCharge = 0;
         correctlyCharged = 0;
         falselyCharged = 0;
-
+		checkChargeCounter = 0;
+		carsToCheck = 0;
 		foreach (Car car in LevelController.CaseData.cars)
 		{
+			carsToCheck++;
 			if (car.batteryLevel < LevelController.CaseData.chargeBound)
 				carsToCharge++;
 		}
 	}
 
     private void CorrectCase()
+    {
+        int chargeBound = LevelController.CaseData.chargeBound;
+
+        if (correctlyCharged == carsToCharge)
+        {
+			if (falselyCharged > 0)
+				PMWrapper.RaiseTaskError ("För många tåg laddades. Bara tåg med batterinivå < " + chargeBound + " ska laddas.");
+			else if (checkChargeCounter < carsToCheck)
+				PMWrapper.RaiseTaskError ("För få batterinivålev kollade. Alla tås batterinivååte kollas fö att vi ska vet om tået ska bli laddat.");
+            else
+                PMWrapper.SetCaseCompleted();
+        }
+        else
+        {
+            if (correctlyCharged + falselyCharged == carsToCharge)
+                PMWrapper.RaiseTaskError("Fel tåg laddades.");
+            else if (correctlyCharged + falselyCharged < carsToCharge)
+                PMWrapper.RaiseTaskError("För få tåg laddades. Alla tåg med batterinivå < " + chargeBound + " ska laddas.");
+            else
+                PMWrapper.RaiseTaskError("För många tåg laddades. Bara tåg med batterinivå < " + chargeBound + " ska laddas.");
+        }
+    }
+}
+
     {
         int chargeBound = LevelController.CaseData.chargeBound;
 
