08270e362dd1f0f4db6da5dbf4015324ffceb00e
Jonathan Bäckström <jonathan.backstrom95@gmail.com>
2018-05-18 10:17:05 +0200

    Update correction in scene 3

--- goods-transport/Assets/Game-2-if/Scripts/SceneController2_3.cs
+++ goods-transport/Assets/Game-2-if/Scripts/SceneController2_3.cs
@@ -14,12 +14,8 @@ public class SceneController2_3 : MonoBehaviour, ISceneController, IPMCompilerSt
 	{
 		if (status == HelloCompiler.StopStatus.Finished)
 		{
-			bool correctSorted = CorrectSorting();
-
-			if (correctSorted)
+			if (CorrectSorting())
 				PMWrapper.SetCaseCompleted();
-			else
-				PMWrapper.RaiseTaskError("Något blev felsorterat.");
 		}
 
 		SortedQueue.ResetQueues();
@@ -27,30 +23,78 @@ public class SceneController2_3 : MonoBehaviour, ISceneController, IPMCompilerSt
 
 	private bool CorrectSorting()
 	{
-		var forwardBounds = LevelController.CaseData.correctSorting.forwardQueue;
-		if (!CorrectQueue(forwardBounds, SortedQueue.ForwardQueue))
-			return false;
+	    var leftBounds = LevelController.CaseData.correctSorting.leftQueue;
+	    if (!CorrectQueue(leftBounds, SortedQueue.LeftQueue, "åt vänster"))
+	        return false;
 
-		var rightBounds = LevelController.CaseData.correctSorting.rightQueue;
-		if (!CorrectQueue(rightBounds, SortedQueue.RightQueue))
-			return false;
+        var forwardBounds = LevelController.CaseData.correctSorting.forwardQueue;
+        if (!CorrectQueue(forwardBounds, SortedQueue.ForwardQueue, "rakt fram"))
+            return false;
 
-		var leftBounds = LevelController.CaseData.correctSorting.leftQueue;
-		if (!CorrectQueue(leftBounds, SortedQueue.LeftQueue))
-			return false;
+        var rightBounds = LevelController.CaseData.correctSorting.rightQueue;
+        if (!CorrectQueue(rightBounds, SortedQueue.RightQueue, "åt höger"))
+            return false;
 
-		return true;
+        return true;
 	}
 
-	private bool CorrectQueue(CorrectSortedQueue bounds, List<GameObject> queue)
+	private bool CorrectQueue(CorrectSortedQueue bounds, List<GameObject> queue, string direction)
 	{
-		foreach (GameObject car in queue)
-		{
-			int itemsInCar = car.GetComponent<CarInfo>().ItemsInCar;
+	    if (!CorrectNumberOfCarsInQueue(bounds, queue, direction))
+	        return false;
 
-			if (itemsInCar > bounds.upperBound || itemsInCar < bounds.lowerBound)
-				return false;
-		}
-		return true;
+	    if (!CorrectBoundsInQueue(bounds, queue, direction))
+	        return false;
+
+        return true;
 	}
+
+    private bool CorrectNumberOfCarsInQueue(CorrectSortedQueue bounds, List<GameObject> queue, string direction)
+    {
+        var correctNumberOfCars = 0;
+        foreach (var car in LevelController.CaseData.cars)
+        {
+            var itemsInCar = 0;
+            foreach (var carSection in car.sections)
+            {
+                itemsInCar += carSection.itemCount;
+            }
+
+            if (itemsInCar <= bounds.upperBound && itemsInCar >= bounds.lowerBound)
+                correctNumberOfCars++;
+        }
+
+        if (queue.Count < correctNumberOfCars)
+        {
+            PMWrapper.RaiseTaskError("För få tåg sorterades " + direction);
+            return false;
+        }
+        if (queue.Count > correctNumberOfCars)
+        {
+            PMWrapper.RaiseTaskError("För många tåg sorterades " + direction);
+            return false;
+        }
+        return true;
+    }
+
+    private bool CorrectBoundsInQueue(CorrectSortedQueue bounds, List<GameObject> queue, string direction)
+    {
+        foreach (GameObject car in queue)
+        {
+            int itemsInCar = car.GetComponent<CarInfo>().ItemsInCar;
+
+            if (itemsInCar > bounds.upperBound)
+            {
+                PMWrapper.RaiseTaskError("I tunneln " + direction + " hamnade tåg med för många varor");
+                return false;
+            }
+
+            if (itemsInCar < bounds.lowerBound)
+            {
+                PMWrapper.RaiseTaskError("I tunneln " + direction + " hamnade tåg med för få varor");
+                return false;
+            }
+        }
+        return true;
+    }
 }
